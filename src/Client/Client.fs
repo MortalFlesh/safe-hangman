213eccc10485800a860ed39cf719c4a69dfe724d
Petr Chromec <mortal_flesh@seznam.cz>
2018-06-17 23:42:33 +0200

    Guess letters in word

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -1,5 +1,6 @@
 module Client
 
+open System
 open Elmish
 open Elmish.React
 
@@ -14,8 +15,15 @@ open Fulma
 type State = Hangman option
 
 type Action =
-| Guess of char
+| Guess of string
 | NewWord of string
+
+type GameProgress =
+| Playing
+| Win
+| Lost
+
+let maxAttempts = 6
 
 let init () : State * Cmd<Action> =
     let state = None
@@ -23,17 +31,34 @@ let init () : State * Cmd<Action> =
     state, cmd
 
 let update (action : Action) (state : State) : State * Cmd<Action> =
-    let model' =
+    let state' =
         match state, action with
-        | _, NewWord w -> Some { word = w; guesses = List.Empty }
-        | Some state, Guess c -> Some { state with guesses = c :: state.guesses }
+        | _, NewWord w -> Some { word = (w.ToUpper ()); guesses = List.Empty }
+        | Some state, Guess g when (g.Length = 1) -> Some { state with guesses = (g.Chars 0 |> Char.ToUpper) :: state.guesses }
+        | Some state, Guess _ -> Some state
         | None, Guess _ -> None
-    model', Cmd.none
+    state', Cmd.none
 
-let show = function
+let showWord = function
 | Some state ->
-    sprintf "%s (%A)" state.word state.guesses
-| None -> "Click the button!"
+    let word = 
+        state.word
+        |> Seq.toList
+        |> Seq.map (fun letter -> if (state.guesses |> Seq.contains letter) then letter.ToString () else "_")
+        |> String.concat " "
+
+    sprintf "%s (DEBUG: %A)"
+    <| word
+    <| state.guesses
+| None -> "Create new word!"
+
+let showGameStatus = function
+| Some state ->
+    match maxAttempts - state.guesses.Length with
+    | remainng when remainng > 1 -> sprintf "Guesses remaing: %d" remainng
+    | 1 -> "Last attempt!"
+    | _ -> "You have lost!"
+| None -> "No word created yet..."
 
 let button txt onClick =
     Button.button
@@ -42,6 +67,11 @@ let button txt onClick =
           Button.OnClick onClick ]
         [ str txt ]
 
+let guessInput onGuess =
+    Input.text [ Input.Props [
+            Value ""
+            OnKeyDown onGuess ] ]
+
 let view (state : State) (dispatch : Action -> unit) =
     div []
         [ Navbar.navbar [ Navbar.Color IsPrimary ]
@@ -49,13 +79,19 @@ let view (state : State) (dispatch : Action -> unit) =
                 [ Heading.h2 [ ]
                     [ str "SAFE - Hangman" ] ] ]
 
-          Container.container []
+          Container.container [ ]
               [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ Heading.h3 [] [ str ("Word: " + show state) ] ]
-                Columns.columns []
-                    [ Column.column [] [ button "-" (fun _ -> dispatch (Guess 'x')) ]
-                      Column.column [] [ button "+" (fun _ -> dispatch (Guess 'y')) ] ] ]
+                    [ Heading.h3 [] [ str (showWord state) ] ]
 
+                Columns.columns [ Columns.IsCentered ]
+                    [ Column.column [ Column.Width (Screen.All, Column.Is2) ] [ guessInput (fun e -> dispatch (Guess (e.key) )) ] ]
+
+                Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
+                    [ Heading.h3 [] [ str (showGameStatus state) ] ]
+
+                Columns.columns [ Columns.IsCentered ]
+                    [ Column.column [ Column.Width (Screen.All, Column.Is2) ] [ button "New word" (fun _ -> dispatch (NewWord "hangman")) ] ]
+              ]
         ]
 
 
